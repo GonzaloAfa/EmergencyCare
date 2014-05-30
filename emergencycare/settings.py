30560a71d182953b1979f6be848d091d30e1f7df
gonzaloafa <gonzaloafa@afachile.cl>
2014-05-30 19:08:17 -0400

    Se añande la libreria datapicker

--- emergencycare/settings.py
+++ emergencycare/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'ficha',
     'bootstrapform',
-)
+    'bootstrap3_datetime',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
